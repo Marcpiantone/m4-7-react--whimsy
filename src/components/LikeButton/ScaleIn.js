0b8eb22022b4c163ba7e6ccb1b1e12aac150c1d7
“Marc <marc_piantone@live.fr>
2020-07-30 09:27:16 -0400

    Answer exercise-6

---
+++ src/components/LikeButton/ScaleIn.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { useSpring, animated } from "react-spring";
+
+const ScaleIn = ({ children }) => {
+  console.log("CALLED");
+  const style = useSpring({
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    position: "relative",
+    transform: "Scale(1)",
+    from: { transform: "scale(0)" },
+    config: {
+      tension: 350,
+      friction: 10,
+    },
+  });
+  return <animated.div style={style}> {children}</animated.div>;
+};
+
+export default ScaleIn;
